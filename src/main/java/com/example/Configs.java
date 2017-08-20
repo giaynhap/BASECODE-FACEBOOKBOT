f718ab107029e817391f99cea5e69209f426432a
Giay Nhap <skincstt@gmail.com>
2017-08-20 12:12:42 +0700

    upload

--- src/main/java/com/example/Configs.java
+++ src/main/java/com/example/Configs.java
@@ -4,5 +4,5 @@ public class Configs {
 	public static String accessToken= "";
 	public static String pageID = "";
 	public static String graphAPI = "https://graph.facebook.com/";	
-	public static String appKey = "giaynhapcoder";
+	public static String appKey = "giaynhap";
 }
